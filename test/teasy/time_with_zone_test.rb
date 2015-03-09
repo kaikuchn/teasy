b5264610b3b63d56070c23eb0792d3eb55a0f77c
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2015-03-09 16:08:44 +0100

    fleshed out tests for constructor

--- test/teasy/time_with_zone_test.rb
+++ test/teasy/time_with_zone_test.rb
@@ -2,7 +2,31 @@ require 'test_helper'
 
 class TimeWithZoneTest < Minitest::Unit::TestCase
   def setup
-    @time = Time.utc(2042, 1, 2, 12, 30, 45, 1.112)
-    @dtime = @time.to_datetime
+    @params = [2042, 4, 2, 8, 30, 45, 1_112]
+    @timestamptz = TimeWithZone.new(*@params)
+  end
+
+  def test_constructor_parameter
+    assert_equal 2042, @timestamptz.year
+    assert_equal 4, @timestamptz.month
+    assert_equal 2, @timestamptz.day
+    assert_equal 8, @timestamptz.hour
+    assert_equal 30, @timestamptz.min
+    assert_equal 45, @timestamptz.sec
+    assert_equal 1_112, @timestamptz.nsec
+  end
+
+  def test_constructor_defaults_to_utc
+    assert_true @timestamptz.utc?
+    assert_equal 'UTC', @timestamptz.zone
+  end
+
+  def test_constructor_applies_provided_timezone
+    timestamptz = TimeWithZone.new(*@params, 'Europe/Berlin')
+    assert_equal 'Europe/Berlin', timestamptz.zone
+    assert_false timestamptz.utc?
+    assert_true timestamptz.dst?
+    assert_equal 7200, timestamptz.offset
+    assert_equal @timestamptz + 7200, timestamptz
   end
 end
