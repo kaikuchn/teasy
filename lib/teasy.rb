3e4590bfde7c3be20dfb0b11a30204c21671b8e6
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2015-03-16 17:34:19 +0100

    added default_zone setter and with_zone yielding

--- lib/teasy.rb
+++ lib/teasy.rb
@@ -5,4 +5,15 @@ module Teasy
   def self.default_zone
     Thread.current[:teasy_default_zone] ||= 'UTC'
   end
+
+  def self.default_zone=(zone)
+    Thread.current[:teasy_default_zone] = zone
+  end
+
+  def self.with_zone(zone)
+    old_zone = Thread.current[:teasy_default_zone]
+    Thread.current[:teasy_default_zone] = zone
+    yield
+    Thread.current[:teasy_default_zone] = old_zone
+  end
 end
---
+++ test/teasy/teasy_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class TeasyTest < Minitest::Unit::TestCase
+  def teardown
+    Teasy.default_zone = 'UTC'
+  end
+
+  def test_default_zone_is_utc
+    assert_equal 'UTC', Teasy.default_zone
+  end
+
+  def test_default_zone_can_be_set
+    assert_equal 'UTC', Teasy.default_zone
+    Teasy.default_zone = 'Asia/Calcutta'
+    assert_equal 'Asia/Calcutta', Teasy.default_zone
+  end
+
+  def test_default_zone_is_thread_safe
+    assert_equal 'UTC', Teasy.default_zone
+    threads = []
+    threads << Thread.new do
+      Teasy.default_zone = 'America/New_York'
+      sleep 0.1
+      assert_equal 'America/New_York', Teasy.default_zone
+    end
+    assert_equal 'UTC', Teasy.default_zone
+    threads << Thread.new do
+      assert_equal 'UTC', Teasy.default_zone
+    end
+    threads.each(&:join)
+  end
+
+  def test_with_zone
+    assert_equal 'UTC', Teasy.default_zone
+    Teasy.with_zone('Europe/Berlin') do
+      assert_equal 'Europe/Berlin', Teasy.default_zone
+    end
+    assert_equal 'UTC', Teasy.default_zone
+  end
+end
